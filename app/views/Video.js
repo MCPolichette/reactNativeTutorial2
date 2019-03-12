40113df210d62cdad414fed2e448af61dd118eca
MCPolichette <mark@polichette.com>
2019-03-12 12:52:46 -0600

    fixxed the google API.  Now I can continue

--- app/views/Video.js
+++ app/views/Video.js
@@ -10,11 +10,10 @@ export class Video extends React.Component {
     constructor(props) {
         super(props);
         this.state = { listLoaded: false };
-
     }
 
     componentDidMount() {
-        return fetch('https://googleapis.com/youtube/v3/search?part=snippet&q=pluralsight&type=video&key=AIzaSyD7tiLmyM47EkLb80n2NGpgZYMvZ3xFziU')
+        return fetch('https://www.googleapis.com/youtube/v3/search?part=snippet&q=pluralsight&type=video&key=AIzaSyD7tiLmyM47EkLb80n2NGpgZYMvZ3xFziU')
             .then((response) => response.json())
             .then((responseJson) => {
                 this.setState({
@@ -26,12 +25,12 @@ export class Video extends React.Component {
                 console.error(error);
             });
     }
+
     render() {
-        const { navigate } = this.props.navigate
+        const { navigate } = this.props.navigation;
         return (
             <View>
                 {this.state.listLoaded && (
-
                     <View style={{ paddingTop: 30 }}>
                         <FlatList
                             data={this.state.videoList}
@@ -40,34 +39,36 @@ export class Video extends React.Component {
                                     navigate={navigate}
                                     id={item.id.videoId}
                                     title={item.snippet.title}
-                                    imageSrc={itmem.snippet.thumbnails.high.url}
+                                    imageSrc={item.snippet.thumbnails.high.url}
                                 />
                             }
                         />
                     </View>
                 )}
+
                 {!this.state.listLoaded && (
                     <View style={{ paddingTop: 30 }}>
                         <Text> LOADING </Text>
                     </View>
                 )}
 
-
             </View>
-        )
+        );
     }
+
 }
+
+
 export class TubeItem extends React.Component {
 
     onPress = () => {
-        this.props.navigate('"VideoDetailRT', { ytubeId: this.props.id });
-
+        this.props.navigate('VideoDetailRT', { ytubeId: this.props.id });
     };
 
     render() {
         return (
             <TouchableWithoutFeedback onPress={this.onPress}>
-                <View stype={{ paddingTop: 20, alignItems: 'center' }}>
+                <View style={{ paddingTop: 20, alignItems: 'center' }}>
                     <Image
                         style={{ width: '100%', height: 200 }}
                         source={{ uri: this.props.imageSrc }}
@@ -79,4 +80,6 @@ export class TubeItem extends React.Component {
             </TouchableWithoutFeedback>
         );
     }
+
 }
+
